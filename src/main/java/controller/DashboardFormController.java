af7ab0389d658a02dff3e203ba4b573b89c6d4ae
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-10-10 04:28:14 +0530

    Login And Dashboard

---
+++ src/main/java/controller/DashboardFormController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardFormController {
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private AnchorPane mainContent;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSetiting(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
+}
